4d206af3598f7880d72d2e77925f25091dd9936e
yite <yite@icloud.com>
2025-05-16 10:47:10 +0800

    modulize framework

--- ems/__init__.py
+++ ems/__init__.py
@@ -2,19 +2,10 @@
 """
 EMS (Exchange Management System) - A tool for managing cryptocurrency exchange operations.
 """
-
-from .config import (
-    Account,
-    ConfigError,
-    ConfigManager,
-)
 from .core import EventExecutor
 from .nats_manager import NatsManager
 
 __all__ = [
-    "ConfigManager",
-    "Account",
-    "ConfigError",
     "NatsManager",
     "EventExecutor",
 ]
