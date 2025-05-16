bd20621e606fa4e1142dc1d69ba5f08ba6381b56
yite <yite@icloud.com>
2025-05-16 10:17:51 +0800

    event handler

---
+++ ems/collection/emitter/example.py
@@ -0,0 +1,15 @@
+import asyncio
+import logging
+import uuid
+from ems.core.emitter import EventEmitter
+
+logger = logging.getLogger(__name__)
+
+class ExampleEmitter(EventEmitter):
+    async def get_generators(self):
+        async def example_generator():
+            while True:
+                event = {'id': str(uuid.uuid4())}
+                await asyncio.sleep(1)  # Emit every second
+                yield event
+        return {'example': example_generator}
