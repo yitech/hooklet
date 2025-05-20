95f5c53648d60ab50c1ae9e227553bbff27f2007
yite <yite@icloud.com>
2025-05-20 13:13:16 +0800

    zmq solution for pilot

--- hooklet/pilot/__init__.py
+++ hooklet/pilot/__init__.py
@@ -1,4 +1,5 @@
 from .inproc_pilot import InProcPilot
 from .nats_pilot import NatsPilot
+from .zmq_pilot import ZmqPilot, ZmqBroker
 
-__all__ = ["NatsPilot", "InProcPilot"]
+__all__ = ["NatsPilot", "InProcPilot", "ZmqPilot", "ZmqBroker"]
