bb8ee08fc71182c551fc9ee0fe5db082633ee237
yite <coastq22889@icloud.com>
2025-06-09 08:58:44 +0800

    add new message type

--- hooklet/types.py
+++ hooklet/types.py
@@ -1,4 +1,20 @@
 from typing import Any, AsyncIterator, Awaitable, Callable
+from pydantic import BaseModel
+import uuid
+import time
 
 MessageHandlerCallback = Callable[[Any], Awaitable[Any]]
 GeneratorFunc = Callable[[], AsyncIterator[dict[str, Any]]]
+
+
+class HookletMessage(BaseModel):
+    id: uuid.UUID = uuid.uuid4()
+    correlation_id: uuid.UUID | None = None
+    target: str | None = None
+    payload: Any
+    created_at: int = lambda: int(time.time() * 1000)
+    start_at: int | None = None
+    finish_at: int | None = None
+
+
+
