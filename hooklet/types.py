37fdf614ccc0b8b1a2293fea0fad45b22a3300fd
yite <coastq22889@icloud.com>
2025-06-09 08:44:23 +0800

    use alternative payload

--- hooklet/types.py
+++ hooklet/types.py
@@ -1,15 +1,17 @@
 import random
 import time
-from typing import Any, AsyncIterator, Awaitable, Callable, Optional
+from typing import Any, AsyncIterator, Awaitable, Callable, Optional, Union
 
 from pydantic import BaseModel, Field
 
 
 class HookletMessage(BaseModel):
+    model_config = {"arbitrary_types_allowed": True}
+    
     id: int = Field(default_factory=lambda: random.randint(1, 10**6 - 1))
     correlation_id: int = Field(default_factory=lambda: random.randint(1, 10**6 - 1))
     target: Optional[str] = None
-    payload: bytearray = Field(default_factory=bytearray)
+    payload: Union[bytes, dict[str, Any]] = Field(default_factory=dict)
     created_at: int = Field(default_factory=lambda: int(time.time() * 1000))
     started_at: int = Field(default_factory=lambda: int(time.time() * 1000))
     finished_at: int = Field(default_factory=lambda: int(time.time() * 1000))
