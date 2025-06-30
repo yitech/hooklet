34f437dbd42dd2b801a91c517ca53b653242d225
yite <coastq22889@icloud.com>
2025-06-30 15:37:39 +0800

    use node module

---
+++ hooklet/node/emmiter.py
@@ -0,0 +1,28 @@
+from hooklet.base.node import Node
+from abc import abstractmethod, ABC
+from typing import Callable, AsyncGenerator
+from hooklet.base.pilot import Msg, PubSub
+import asyncio
+
+class Emitter(Node, ABC):
+    def __init__(self, name: str, pubsub: PubSub, router: Callable[[Msg], str]):
+        """
+        Emitter node that emits messages to the router.
+        """
+        super().__init__(name)
+        self.pubsub = pubsub
+        self.router = router
+    
+
+    @abstractmethod
+    async def emitter(self) -> AsyncGenerator[Msg, None]:
+        """
+        Use is_running to stop the emitter.
+        """
+        raise NotImplementedError("Subclasses must implement emitter()")
+
+    async def run(self):
+        while self.is_running:
+            async for msg in self.emitter():
+                subject = self.router(msg)
+                await self.pubsub.publish(subject, msg)
