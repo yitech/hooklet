15d856447b4964dafb042eb00cc224850aa723e8
yite <coastq22889@icloud.com>
2025-06-30 08:28:17 +0800

    verify the duplication

--- hooklet/utils/header_validator.py
+++ hooklet/utils/header_validator.py
@@ -4,9 +4,8 @@ from typing import Any, Dict, Optional, List, Union, AsyncGenerator
 from hooklet.base import Pilot
 from hooklet.base.types import Headers, Msg
 from hooklet.logger import get_logger
-from aiocache import cached
-from aiocache.serializers import JsonSerializer
-from cachetools import TTLCache
+from aiocache import Cache
+
 
 logger = get_logger(__name__)
 
@@ -32,14 +31,12 @@ class HeaderValidator:
         self._forward_tasks: List[asyncio.Task] = []
         self._validation_errors: List[str] = []
 
-        self._cache = TTLCache(maxsize=1000, ttl=60)
+        self._cache = Cache(method=Cache.METHOD_MEMORY, ttl=60)
 
     @asynccontextmanager
     async def validate(self, data: Msg, headers: Headers) -> AsyncGenerator["HeaderValidator", None]:
         self._headers = self._validate_headers(headers)
-        await self._preprocess(data)
         yield self
-        await self._postprocess(data)
         
     def _validate_headers(self, headers: Headers) -> Headers:
         """Validate all headers and collect any errors."""
@@ -61,19 +58,18 @@ class HeaderValidator:
         logger.warning(f"Validation errors: {self._validation_errors}")
         return headers
     
-    async def preprocess(self, data: Msg) -> bool:
-        """Preprocess headers."""
-        # Check if trace ID is duplicated
-        if self.TRACE_ID in data.headers and self._cache.get(data.headers[self.TRACE_ID]):
+    async def is_duplicated(self) -> bool:
+        """Check if the trace ID is duplicated."""
+        if self.TRACE_ID in self._headers:
+            if await self._cache.get(self._headers[self.TRACE_ID]):
+                return True
+            await self._cache.set(self._headers[self.TRACE_ID], True)
             return False
-        return True
+        return False
     
-    async def run(self, task: asyncio.Task) -> Dict[str, Any]:
-        """Run the header validator."""
-        await task
+    async def forward_to(self, data: Msg, headers: Headers) -> None:
+        """Forward the message to the specified topic."""
+        if self.PUBSUB_FORWARD_TO in headers:
+            await self._pilot.pubsub().publish(headers[self.PUBSUB_FORWARD_TO], data, headers)
     
-    async def postprocess(self, data: Msg, headers: Headers) -> None:
-        """Postprocess headers."""
-        if self.PUBSUB_FORWARD_TO in self._headers:
-            await self._pilot.pubsub().publish(self._headers[self.PUBSUB_FORWARD_TO], data, headers)
         
