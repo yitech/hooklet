4628facbcd78322d08065b1183df7402ebfbad2c
yite <coastq22889@icloud.com>
2025-07-01 08:55:20 +0800

    use aclose

--- hooklet/node/emmiter.py
+++ hooklet/node/emmiter.py
@@ -2,6 +2,7 @@ from hooklet.base.node import Node
 from abc import abstractmethod, ABC
 from typing import Callable, AsyncGenerator
 from hooklet.base.pilot import Msg, PubSub
+from contextlib import aclosing
 import asyncio
 
 class Emitter(Node, ABC):
@@ -22,20 +23,13 @@ class Emitter(Node, ABC):
         raise NotImplementedError("Subclasses must implement emit()")
 
     async def run(self):
-        emit_gen = None
         try:
-            emit_gen = self.emit()
-            async for msg in self.emit():
-                if self.shutdown_event.is_set():
-                    break
-                subject = self.router(msg)
-                await self.pubsub.publish(subject, msg)
+            async with aclosing(self.emit()) as gen:
+                async for msg in gen:
+                    if self.shutdown_event.is_set():
+                        break
+                    subject = self.router(msg)
+                    await self.pubsub.publish(subject, msg)
         except Exception as e:
             await self.on_error(e)
-        finally:
-            # Ensure the generator is properly closed
-            if emit_gen is not None:
-                try:
-                    await emit_gen.aclose()
-                except:
-                    pass
+        
